8072f77041cf0bc6985db10375b80ab4032f3e1e
thibautloiseau <thibaut.loiseau@gmail.com>
2021-06-23 17:23:29 +0200

    Simplifying heavy calculus

--- data.py
+++ data.py
@@ -14,15 +14,16 @@ class ReshapeTransform:
 
 class ReshapeTransformTarget:
     """Transform target output in the appropriate way"""
-    def __init__(self, noClasses, expandOutput):
+    def __init__(self, noClasses, expandOutput, maxIntState):
         self.noClasses = noClasses
         self.expandOutput = expandOutput
+        self.maxIntState = maxIntState
 
     def __call__(self, target):
         target = torch.tensor(target).unsqueeze(0).unsqueeze(1)
         targetOneHot = torch.zeros((1, self.noClasses))
 
-        return targetOneHot.scatter_(1, target, 1).repeat_interleave(self.expandOutput).squeeze(0)
+        return targetOneHot.scatter_(1, target, 1).repeat_interleave(self.expandOutput).squeeze(0) * self.maxIntState
 
 
 # For fc architecture
@@ -34,6 +35,7 @@ class Data_Loader(DataLoader):
         self.fcTransforms = [transforms.ToTensor(), ReshapeTransform((-1,))]
         self.expandOutput = args.expandOutput
         self.dataset = args.dataset
+        self.maxIntState = 2**(args.bitsState - 1) - 1
 
     def __call__(self):
         """We return a tuple with both dataloader for train and test"""
@@ -43,7 +45,7 @@ class Data_Loader(DataLoader):
                     datasets.MNIST(
                         root='./data', train=True, download=True,
                         transform=transforms.Compose(self.fcTransforms),
-                        target_transform=ReshapeTransformTarget(10, self.expandOutput)
+                        target_transform=ReshapeTransformTarget(10, self.expandOutput, self.maxIntState)
                     ),
                     shuffle=True, batch_size=self.trainBatchSize, num_workers=2, pin_memory=True
                 ),
@@ -52,7 +54,7 @@ class Data_Loader(DataLoader):
                     datasets.MNIST(
                         root='./data', train=False, download=True,
                         transform=transforms.Compose(self.fcTransforms),
-                        target_transform=ReshapeTransformTarget(10, self.expandOutput)
+                        target_transform=ReshapeTransformTarget(10, self.expandOutput, self.maxIntState)
                     ),
                     shuffle=True, batch_size=self.testBatchSize, num_workers=2, pin_memory=True
                 )
--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from network import *
 
 parser = argparse.ArgumentParser(description='Binary Equilibrium Propagation')
 
-# For 1 hiddent layer with augmented output
+# For 1 hidden layer with augmented output
 # --device 0 --dataset MNIST --archi fc --binarySettings WA --layersList 784 8192 100 --expandOutput 10 --T 20 --Kmax 10
 # --beta 2 --randomBeta 1 --gamma 2e-6 2e-6 --tau 2.5e-7 2e-7 --lrBias 1e-7 1e-7 --trainBatchSize 64 --testBatchSize 512
 # --epochs 100 --learnAlpha 0
@@ -63,7 +63,7 @@ parser.add_argument(
     '--beta',
     type=float,
     default=2,
-    help='nudging parameter (default: 1)')
+    help='Nudging parameter (default: 2)')
 parser.add_argument(
     '--randomBeta',
     type=int,
@@ -73,14 +73,14 @@ parser.add_argument(
     '--gammaInt',
     nargs='+',
     type=float,
-    default=[4, 4],
+    default=[10, 10],
     help='Low-pass filter constant of BOP for int layers, a power of 2')
 parser.add_argument(
     '--tauInt',
     nargs='+',
     type=float,
-    default=[20, 40],
-    help='Thresholds used for the binary optimization in BOP for int layers')
+    default=[600, 300],
+    help='Thresholds used for the binary optimization in BOP')
 # Training settings
 parser.add_argument(
     '--hasBias',
@@ -91,7 +91,7 @@ parser.add_argument(
     '--lrBias',
     nargs='+',
     type=float,
-    default=[4, 4],
+    default=[17, 17],
     help='Learning rates for bias')
 parser.add_argument(
     '--epochs',
@@ -99,23 +99,26 @@ parser.add_argument(
     default=100,
     metavar='N',
     help='number of epochs to train (default: 2)')
-# Learning the scaling factor
 parser.add_argument(
-    '--learnAlpha',
-    type=int,
-    default=0,
-    help='Learn the scaling factors or let them fixed (default: 1, other: 0)')
-parser.add_argument(
-    '--lrAlpha',
-    nargs='+',
-    type=float,
-    default=[1, 1],
-    help='learning rates for the scaling factors')
-parser.add_argument(
-    '--nbBits',
+    '--bitsState',
     type=int,
     default=13,
     help='Number of bits for states in signed int coding')
+parser.add_argument(
+    '--bitsW',
+    type=int,
+    default=20,
+    help='Number of bits for weights gradients in signed int coding')
+parser.add_argument(
+    '--bitsBias',
+    type=int,
+    default=24,
+    help='Number of bits for biases gradients in signed int coding')
+parser.add_argument(
+    '--decay',
+    type=int,
+    default=2,
+    help='Quantity by which we multiply the threshold for BOP')
 
 args = parser.parse_args()
 
@@ -123,58 +126,56 @@ if __name__ == '__main__':
     # We reverse the layersList according to the convention that the output is 0 indexed
     args.layersList.reverse()
 
-    for i in range(5, 10):
-        args.lrBias = [i, i]
-        # Initializing the data and the network
-        trainLoader, testLoader = Data_Loader(args)()
+    # Initializing the data and the network
+    trainLoader, testLoader = Data_Loader(args)()
 
-        net = FCbinWAInt(args)
+    net = FCbinWAInt(args)
 
-        # Create visualizer for tensorboard and save training
-        visualizer = Visualizer(net, args)
-        visualizer.saveHyperParameters()
+    # Create visualizer for tensorboard and save training
+    visualizer = Visualizer(net, args)
+    visualizer.saveHyperParameters()
 
-        if net.cuda:
-            net.to(net.device)
+    if net.cuda:
+        net.to(net.device)
 
-        print("Running on " + net.deviceName)
+    print("Running on " + net.deviceName)
 
-        # Training and testing the network
-        for epoch in tqdm(range(args.epochs)):
-            print("\nStarting epoch " + str(epoch + 1) + "/" + str(args.epochs))
+    # Training and testing the network
+    for epoch in tqdm(range(args.epochs)):
+        print("\nStarting epoch " + str(epoch + 1) + "/" + str(args.epochs))
 
-            # Training
-            print("Training")
-            nbChanges, aveTrainError, singleTrainError, trainLoss, _ = trainFC(net, trainLoader, args)
+        # Training
+        print("Training")
+        nbChanges, aveTrainError, singleTrainError, trainLoss, _ = trainFC(net, trainLoader, epoch, args)
 
-            visualizer.addTraining(aveTrainError, singleTrainError, trainLoss, epoch)
-            visualizer.addNbChanges(nbChanges, epoch)
+        visualizer.addTraining(aveTrainError, singleTrainError, trainLoss, epoch)
+        visualizer.addNbChanges(nbChanges, epoch)
 
-            # Testing
-            print("Testing")
-            aveTestError, singleTestError, testLoss = testFC(net, testLoader, args)
-            visualizer.addTesting(aveTestError, singleTestError, testLoss, epoch)
+        # Testing
+        print("Testing")
+        aveTestError, singleTestError, testLoss = testFC(net, testLoader, args)
+        visualizer.addTesting(aveTestError, singleTestError, testLoss, epoch)
 
-            print("Training loss: " + str(trainLoss))
-            print("Average training error: " + str(aveTrainError))
-            print("Single training error: " + str(singleTrainError))
+        print("Training loss: " + str(trainLoss))
+        print("Average training error: " + str(aveTrainError))
+        print("Single training error: " + str(singleTrainError))
 
-            print("Testing loss: " + str(testLoss))
-            print("Average testing error: " + str(aveTestError))
-            print("Single testing error: " + str(singleTestError))
+        print("Testing loss: " + str(testLoss))
+        print("Average testing error: " + str(aveTestError))
+        print("Single testing error: " + str(singleTestError))
 
-            # Save checkpoint after epoch
-            print("Saving checkpoint")
+        # Save checkpoint after epoch
+        print("Saving checkpoint")
 
-            torch.save({
-                'epoch': epoch,
-                'modelStateDict': net.state_dict(),
-                'trainLoss': trainLoss,
-                'aveTrainError': aveTrainError,
-                'testLoss': testLoss,
-                'aveTestError': aveTestError,
-            }, os.path.join(visualizer.path, 'checkpoint.pt'))
+        torch.save({
+            'epoch': epoch,
+            'modelStateDict': net.state_dict(),
+            'trainLoss': trainLoss,
+            'aveTrainError': aveTrainError,
+            'testLoss': testLoss,
+            'aveTestError': aveTestError,
+        }, os.path.join(visualizer.path, 'checkpoint.pt'))
 
-        print("Finished training")
+    print("Finished training")
 
 
